b206bcc377742be2ad1f2c7b65f25e1e1d6d6ccd
Mohammad Khoulani <mhmmdkholani@gmail.con=m>
2021-06-27 20:22:36 +0300

    Add auth service to startup class and init the database with sql server and entity framework

--- src/AspCoreDemo.Api/Startup.cs
+++ src/AspCoreDemo.Api/Startup.cs
@@ -1,7 +1,10 @@
+using AspCoreDemo.Api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,19 @@ namespace AspCoreDemo.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // configure EntityFramework with SQL server 
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequiredLength = 5;
+            }).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
